4f46640c17986b4df02b4ef78ed0030befa28b9c
leanl_000 <leanl_000@TOSHIBA>
2016-09-30 09:31:51 +0100

    Kata2: Version 4

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -7,10 +7,11 @@ public class Kata2 {
 
     public static void main(String[] args) {
         
-    final int[] data = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
+    //final Integer[] data = {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
+    String[] data = {"pepe", "juan", "tomas", "pepe", "tomas", "rosa", "juanito", "alexis", "ana"};
     Histogram histo = new Histogram(data);
-    Map<Integer,Integer> histogr = histo.getHistogram();
-    for (int key : histogr.keySet()) {
+    Map<String,Integer> histogr = histo.getHistogram();
+    for (String key : histogr.keySet()) {
             System.out.println(key + " --> " +histogr.get(key));
         }
     }
